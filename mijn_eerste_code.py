e42cdc283ff1dc9b7d9775d51a83e8ce74a2bd68
JesseVerbeeck <jesse.verbeeck@student.kuleuven.be>
2025-05-18 13:57:31 +0200

    Update mijn_eerste_code.py

--- mijn_eerste_code.py
+++ mijn_eerste_code.py
@@ -18,3 +18,14 @@ for j in range(2, 101):
         print(f"{j} is een priemgetal.")
     else:
         print(f"{j} is geen priemgetal.")
+
+
+for i in range(1, 101):
+    if i % 3 == 0 and i % 5 == 0:
+        print("FizzBuzz")
+    elif i % 3 == 0:
+        print("Fizz")
+    elif i % 5 == 0:
+        print("Buzz")
+    else:
+        print(i)
